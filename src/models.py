6c1e1b12cbae6ba5e54c9b6305b5e66c0674c99e
Antonio Whitbeck <a.whitbeck89@gmail.com>
2024-09-10 20:21:33 +0000

    finished

--- src/models.py
+++ src/models.py
@@ -15,18 +15,22 @@ class User(Base):
     firstname = Column(String(250), unique=False, nullable=False)
     lastname = Column(String(250), unique=False, nullable=False)
     email = Column(String(250), unique=True, nullable=False)
+    followers = relationship('Follower')
+    comments = relationship('Comment')
+    posts = relationship('Post')
 
 class Follower(Base):
     __tablename__ = 'follower'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship('User')
 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    user = relationship('User')
+    comments = relationship('Comment')
 
 class Comment(Base):
     __tablename__ = 'comment'
@@ -34,40 +38,15 @@ class Comment(Base):
     comment_text = Column(String(250), unique=False, nullable=False)
     user_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship(Post)
-    user = relationship(User)
+    post = relationship('Post')
+    user = relationship('User')
 
 class Media(Base):
     __tablename__ = 'media'
-    id = Column(Integer, pimary_key=True)
+    id = Column(Integer, primary_key=True)
     url = Column(String(250), unique=True, nullable=False)
     post_id = Column(Integer, ForeignKey('post.id'))
-
-
-
-
-# class Person(Base):
-#     __tablename__ = 'person'
-#     # Here we define columns for the table person
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(250), nullable=False)
-
-# class Address(Base):
-#     __tablename__ = 'address'
-#     # Here we define columns for the table address.
-#     # Notice that each column is also a normal Python instance attribute.
-#     id = Column(Integer, primary_key=True)
-#     street_name = Column(String(250))
-#     street_number = Column(String(250))
-#     post_code = Column(String(250), nullable=False)
-#     person_id = Column(Integer, ForeignKey('person.id'))
-#     person = relationship(Person)
-
-#     def to_dict(self):
-#         return {}
-
-## Draw from SQLAlchemy base
+    post = relationship('Post')
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
